f6d8e843c717fbd308832d996d720944cef8fced
kldhs <listom@sina.com>
2021-08-24 17:21:43 +0800

    mqtt

--- src/main/java/com/utils/threadpool/Test.java
+++ src/main/java/com/utils/threadpool/Test.java
@@ -1,9 +1,21 @@
 package com.utils.threadpool;
 
+import com.utils.spring.SpringBootUtil;
+import com.utils.threadpool.executorsandspring.AsyncService;
+
 /**
  * @author xs
  * @date 2021/8/19 9:08
+ * 线程池
  */
 public class Test {
-
+    public static void main(String[] args) {
+        for (int i = 0; i <=100 ; i++) {
+            AsyncService asyncService = (AsyncService) SpringBootUtil.getBean(AsyncService.class);
+            System.err.println(i+"----start submit");
+            //调用service层的任务
+            asyncService.executeAsync();
+            System.err.println(i+"----end submit");
+        }
+    }
 }
