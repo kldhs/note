bffb4af5331bdc2cb415c36d9757b82fb470c0f4
kldhs <listom@sina.com>
2022-03-28 11:00:46 +0800

    spring启动时不加载mysql相关配置

--- framework/src/main/java/com/utils/FrameworkApplication.java
+++ framework/src/main/java/com/utils/FrameworkApplication.java
@@ -1,22 +1,32 @@
 package com.utils;
 
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
 import com.utils.spring.SpringBootUtil;
-import org.mybatis.spring.annotation.MapperScan;
+import lombok.SneakyThrows;
+import org.eclipse.leshan.core.model.InvalidDDFFileException;
+import org.eclipse.leshan.core.model.InvalidModelException;
+import org.eclipse.paho.client.mqttv3.MqttException;
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.annotation.Resource;
+import java.io.IOException;
 
-@SpringBootApplication(
-        //exclude= {DataSourceAutoConfiguration.class}
-        )
-@MapperScan({"com.utils.mysql.mapper"})
+@SpringBootApplication(exclude = {
+        //DataSourceAutoConfiguration.class,
+        //DataSourceTransactionManagerAutoConfiguration.class,
+        //DruidDataSourceAutoConfigure.class ,
+        //HibernateJpaAutoConfiguration.class
+})
+//@MapperScan({"com.utils.mysql.mapper"})
 @EnableAsync
 //开启定时任务
 @EnableScheduling
